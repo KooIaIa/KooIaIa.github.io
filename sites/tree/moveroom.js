ddb114fef8c8c0453129162432a3cceef4ae81e8
KooIaIa <KooIaIa@users.noreply.github.com>
2017-08-13 03:23:56 -0400

    added tree site

---
+++ sites/tree/moveroom.js
@@ -0,0 +1,18 @@
+room.onLoad = function()
+{
+	for (obj in room.objects)
+	{
+		//obj.pos = Vector(player.pos) + Vector(obj.pos)
+	}
+	
+	/*
+	var group = ['js_image1', 'js_image2', 'js_image3'];
+    room.objects["object_js_id_here"].pos = Vector(player.pos);
+	for (obj_id in group)
+	{
+    room.objects[obj_id].pos = Vector(player.pos);
+	}
+	*/
+
+}
+
